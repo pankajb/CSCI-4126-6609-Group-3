85549c7042bf50d0c0db7173b1384690bc4e7515
PankajB <mr.pankajbahekar@gmail.com>
2014-07-10 20:12:21 -0300

    Server.js
    Code Cleanup

--- GreenHouseDrupal/nodeJsRestAPI/server.js
+++ GreenHouseDrupal/nodeJsRestAPI/server.js
@@ -17,7 +17,7 @@ var port = process.env.PORT || 8080; 		// set our port
 var mongoose   = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/Group3'); // connect to our database
 
-//var Bear     = require('./app/models/bear');
+
 var Group1SpecificData = require('./app/models/group1SpecificData');
 var Group2SpecificData = require('./app/models/group2SpecificData');
 var Group3SpecificData = require('./app/models/group3SpecificData');
@@ -44,84 +44,7 @@ var router = express.Router(); 				// get an instance of the express Router
 router.get('/', function(req, res) {
 	res.json({ message: 'hooray! welcome to our api!' });	
 });
-/*
-// more routes for our API will happen here
-router.route('/bears')
 
-	// create a bear (accessed at POST http://localhost:8080/api/bears)
-	.post(function(req, res) {
-		
-		var bear = new Bear(); 		// create a new instance of the Bear model
-		bear.name = req.body.name;  // set the bears name (comes from the request)
-
-		// save the bear and check for errors
-		bear.save(function(err) {
-			if (err)
-				res.send(err);
-
-			res.json({ message: 'Bear created!' });
-		});
-		
-	})
-
-	// get all the bears (accessed at GET http://localhost:8080/api/bears)
-	.get(function(req, res) {
-		Bear.find(function(err, bears) {
-			if (err)
-				res.send(err);
-
-			res.json(bears);
-		});
-	});
-
-
-	
-	router.route('/bears/:bear_id')
-
-	// get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
-	.get(function(req, res) {
-		Bear.findById(req.params.bear_id, function(err, bear) {
-			if (err)
-				res.send(err);
-			res.json(bear);
-		});
-	})
-	
-		// update the bear with this id (accessed at PUT http://localhost:8080/api/bears/:bear_id)
-	.put(function(req, res) {
-
-		// use our bear model to find the bear we want
-		Bear.findById(req.params.bear_id, function(err, bear) {
-
-			if (err)
-				res.send(err);
-
-			bear.name = req.body.name; 	// update the bears info
-
-			// save the bear
-			bear.save(function(err) {
-				if (err)
-					res.send(err);
-
-				res.json({ message: 'Bear updated!' });
-			});
-
-		});
-	})
-	
-		// delete the bear with this id (accessed at DELETE http://localhost:8080/api/bears/:bear_id)
-	.delete(function(req, res) {
-		Bear.remove({
-			_id: req.params.bear_id
-		}, function(err, bear) {
-			if (err)
-				res.send(err);
-
-			res.json({ message: 'Successfully deleted' });
-		});
-	});
-
-	*/
 	// ..............................................................................................group1SpecificData api
 router.route('/group1SpecificData')
 
